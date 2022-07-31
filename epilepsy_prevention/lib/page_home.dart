c38dc1124e3ea4fc08987b92282898ac76cf80d6
elliotharding <elliot.one.harding@gmail.com>
2022-07-31 19:28:43 +0100

    wip - starting on clean home page

---
+++ epilepsy_prevention/lib/page_home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class PageHome extends StatefulWidget
+{
+  const PageHome({Key? key}) : super(key: key);
+
+  @override
+  State<PageHome> createState() => PageHomeState();
+}
+
+class PageHomeState extends State<PageHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold
+    (
+
+    );
+  }
+}
