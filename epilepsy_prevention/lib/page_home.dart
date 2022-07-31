63d1680a046564ee8a17a79f60df0d31a8853a9a
elliotharding <elliot.one.harding@gmail.com>
2022-07-31 19:54:12 +0100

    Checkbox and text

--- epilepsy_prevention/lib/page_home.dart
+++ epilepsy_prevention/lib/page_home.dart
@@ -8,12 +8,22 @@ class PageHome extends StatefulWidget
   State<PageHome> createState() => PageHomeState();
 }
 
-class PageHomeState extends State<PageHome> {
+class PageHomeState extends State<PageHome>
+{
+  bool? m_bAppEnabled = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold
-    (
-
+    return Scaffold(
+      body: Row(children: <Widget>[
+            const Text("Enable: ", style: TextStyle(fontSize: 10)),
+            Checkbox(value: m_bAppEnabled,
+                onChanged: (bool? value){
+                  setState((){
+                    m_bAppEnabled = value;
+                  });})
+          ]
+          )
     );
   }
 }
