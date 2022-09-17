22a3b52beb88a792e77044afc331914eb8cd0884
elliotharding <elliot.one.harding@gmail.com>
2022-09-17 03:31:18 +0100

    wip - broken - starting on notifications

--- epilepsy_prevention/lib/main.dart
+++ epilepsy_prevention/lib/main.dart
@@ -3,14 +3,48 @@ import 'package:flutter/material.dart';
 import 'package:epilepsy_prevention/memory.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:hive/hive.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+//Timezone imports
+import 'package:timezone/data/latest_all.dart' as timeZone;
+import 'package:timezone/timezone.dart' as timeZone;
+import 'package:flutter_native_timezone/flutter_native_timezone.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  //Setup database stuff
   final appDocumentDirectory = await getApplicationDocumentsDirectory();
   Hive.init(appDocumentDirectory.path);
   Hive.registerAdapter(MemoryAdapter());
   var database = Database();
   await database.init();
+
+  setupTimezoneStuff();
+
+  //App launched by notification?
+  String? selectedNotificationPayload;
+  final NotificationAppLaunchDetails? notificationAppLaunchDetails = await flutterLocalNotificationsPlugin.getNotificationAppLaunchDetails();
+  if (notificationAppLaunchDetails?.didNotificationLaunchApp ?? false) {
+    selectedNotificationPayload = notificationAppLaunchDetails!.payload;
+  }
+
+  //Initalize local notifications plugin
+  await flutterLocalNotificationsPlugin.initialize(const InitializationSettings(android: AndroidInitializationSettings('ic_launcher.png')),
+      onSelectNotification: (String? payload) async {
+        selectedNotificationPayload = payload;
+      });
+
+  //Check if notification permissions are granted, if not ask.
+  bool? notificationPermissionGranted = false;
+  if(!(await isAndroidNotificationPermissionGranted()))
+  {
+    notificationPermissionGranted = await requestPermissionsForAndroid();
+  }
+
+  //Run app
   runApp(const App());
 }
 
@@ -26,4 +60,28 @@ class App extends StatelessWidget
         home: PageHome()
       );
   }
+}
+
+Future<bool> isAndroidNotificationPermissionGranted() async {
+  return await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+      AndroidFlutterLocalNotificationsPlugin>()
+      ?.areNotificationsEnabled() ??
+      false;
+}
+
+Future<bool?> requestPermissionsForAndroid() async
+{
+  final AndroidFlutterLocalNotificationsPlugin? androidImplementation =
+  flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<
+      AndroidFlutterLocalNotificationsPlugin>();
+
+  return await androidImplementation?.requestPermission();
+}
+
+Future<void> setupTimezoneStuff() async
+{
+  timeZone.initializeTimeZones();
+  final String? timeZoneName = await FlutterNativeTimezone.getLocalTimezone();
+  timeZone.setLocalLocation(timeZone.getLocation(timeZoneName!));
 }
