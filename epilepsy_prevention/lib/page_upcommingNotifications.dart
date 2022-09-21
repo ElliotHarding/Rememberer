7c6e2fd6c1edc01c56fd5fff1118875f066a9a14
Elliot <elliot.one.harding@gmail.com>
2022-09-21 18:44:11 +0100

    wip - ui - progress

--- epilepsy_prevention/lib/page_upcommingNotifications.dart
+++ epilepsy_prevention/lib/page_upcommingNotifications.dart
@@ -2,6 +2,7 @@ import 'package:epilepsy_prevention/notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:epilepsy_prevention/memory.dart';
 import 'package:epilepsy_prevention/page_test.dart';
+import 'package:epilepsy_prevention/page_memory.dart';
 
 class PageUpcommingNotifications extends StatefulWidget
 {
@@ -36,10 +37,65 @@ class PageUpcommingNotificationsState extends State<PageUpcommingNotifications>
 
           const Spacer(),
 
-          const Text( "Enable: ", style: TextStyle(fontSize: 10), textAlign: TextAlign.center),
+          SizedBox(width: MediaQuery.of(context).size.width, height: MediaQuery.of(context).size.height, child: ListView(
+              children: getNotificationWidgets(context))
+          ),
 
           const Spacer()
         ],)
     );
   }
+
+  List<Widget> getNotificationWidgets(BuildContext context)
+  {
+
+
+    List<MemoryNotification> notifications = <MemoryNotification>[];
+
+    var box = Database().getMemoryBox();
+    if(box != null)
+    {
+      for(Memory memory in box.values)
+      {
+        for (int notifyTime in memory.m_notifyTimes)
+        {
+          notifications.add(MemoryNotification(notifyTime, memory));
+        }
+      }
+    }
+
+    notifications.sort((a, b) => a.m_notificationTime.compareTo(b.m_notificationTime));
+
+    List<Widget> widgets = <Widget>[];
+    for(MemoryNotification memNotification in notifications)
+    {
+      widgets.add(Row(mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          TextButton(onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context) => PageMemory(memNotification.m_memory)));
+          }, child: Text(memNotification.m_memory.m_question, style: const TextStyle(fontSize: 20.0, color: Colors.blue))),
+
+          Text(memNotification.m_notificationTime.toString(), style: const TextStyle(fontSize: 20.0, color: Colors.blue)),
+
+          TextButton(onPressed: () {
+
+          }, child: const Text("X"))
+        ],
+      ));
+    }
+
+    return widgets;
+  }
+}
+
+class MemoryNotification
+{
+  MemoryNotification(int notificationTime, Memory memory)
+  {
+    m_notificationTime = notificationTime;
+    m_memory = memory;
+  }
+
+  int m_notificationTime = 0;
+  Memory m_memory = Memory();
 }
