e66e14d9a2fc011c79ebf014294b984fce8679f4
Elliot <elliot.one.harding@gmail.com>
2022-09-23 22:00:20 +0100

    wip - progress

--- epilepsy_prevention/lib/page_memoryReminders.dart
+++ epilepsy_prevention/lib/page_memoryReminders.dart
@@ -19,6 +19,11 @@ class PageMemoryRemindersState extends State<PageMemoryReminders>
 {
   Memory m_memoryBefore = Memory();
 
+  final m_maxNotificationsController = TextEditingController();
+
+  double m_startIteration = 0;
+  double m_maxNotifications = 0;
+
   Widget build(BuildContext context)
   {
     Notifications.m_selectedNotificationSubject.stream.listen((String? memoryKey) async {
@@ -60,6 +65,36 @@ class PageMemoryRemindersState extends State<PageMemoryReminders>
                   }
                 });
               }
+          )),
+
+          const Spacer(),
+
+          SizedBox(width: MediaQuery.of(context).size.width * 0.9, height: 70, child: Row(children : [
+            const Text("Max Notifications: ", style: TextStyle(fontSize: 30, color: Colors.blue), textAlign: TextAlign.left),
+            SizedBox(width: 70, height: 70, child:
+              TextField(
+                decoration: const InputDecoration(border: OutlineInputBorder(), hintText: 'Enter wrong answers.'),
+                style: const TextStyle(fontSize: 30, color: Colors.black),
+                controller: m_maxNotificationsController,
+                keyboardType: TextInputType.number
+              )
+            )
+          ])),
+
+          const Spacer(),
+
+          SizedBox(width: MediaQuery.of(context).size.width * 0.9, height: 35, child: const Text("Current Notification", style: TextStyle(fontSize: 30, color: Colors.blue), textAlign: TextAlign.left)),
+
+          SizedBox(width: MediaQuery.of(context).size.width * 0.9, height: 35, child:
+            Slider(
+              value: m_startIteration,
+              min: 0,
+              max: m_maxNotifications,
+              onChanged: (newValue) {
+                setState(() {
+                  m_startIteration = newValue;
+                });
+              },
           )),
 
           const Spacer(),
