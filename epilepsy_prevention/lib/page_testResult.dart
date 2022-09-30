3d21fd7f649b7d2aecce3369a6b3de039493a3d1
Elliot <elliot.one.harding@gmail.com>
2022-09-30 19:08:56 +0100

    wip - ui - good progress

--- epilepsy_prevention/lib/page_testResult.dart
+++ epilepsy_prevention/lib/page_testResult.dart
@@ -16,40 +16,40 @@ class PageTestResult extends StatelessWidget
     Notifications.setupNotificationActionListener(context);
 
     return WillPopScope(onWillPop: () async {Navigator.push(context, MaterialPageRoute(builder: (context) => PageMemories())); return true;}, child: Scaffold(body:
-    Column(children: <Widget>[
+      ListView(shrinkWrap: true, children: <Widget>[
 
-      const Spacer(),
+        SizedBox(width: MediaQuery.of(context).size.width, height: MediaQuery.of(context).size.height * 0.25, child:
+          Center(child:
+            Text(m_bSuccess ? "Correct!" : "Wrong!", style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.blue), textAlign: TextAlign.center)
+        )),
 
-      SizedBox(width: MediaQuery.of(context).size.width, height: MediaQuery.of(context).size.height * 0.25, child:
-        Text(m_bSuccess ? "Correct!" : "Wrong!", style: const TextStyle(fontSize: 30), textAlign: TextAlign.center)
-      ),
+        SizedBox(width: MediaQuery.of(context).size.width, child:
+          const Text("Question", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.blue), textAlign: TextAlign.center)
+        ),
 
-      SizedBox(width: MediaQuery.of(context).size.width, height: MediaQuery.of(context).size.height * 0.1, child:
-        const Text("Question", style: TextStyle(fontSize: 30, color: Colors.blue), textAlign: TextAlign.center)
-      ),
+        SizedBox(width: MediaQuery.of(context).size.width, height: MediaQuery.of(context).size.height * 0.25, child:
+          Text(m_memory.m_question, style: const TextStyle(fontSize: 30), textAlign: TextAlign.center)
+        ),
 
-      SizedBox(width: MediaQuery.of(context).size.width, height: MediaQuery.of(context).size.height * 0.1, child:
-        Text(m_memory.m_question, style: const TextStyle(fontSize: 30), textAlign: TextAlign.center)
-      ),
+        SizedBox(width: MediaQuery.of(context).size.width, child:
+          const Text("Answer", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.blue), textAlign: TextAlign.center)
+        ),
 
-      const Spacer(),
+        SizedBox(width: MediaQuery.of(context).size.width, height: MediaQuery.of(context).size.height * 0.25, child:
+          Text(m_memory.m_answer, style: const TextStyle(fontSize: 30), textAlign: TextAlign.center)
+        ),
 
-      SizedBox(width: MediaQuery.of(context).size.width, height: MediaQuery.of(context).size.height * 0.1, child:
-        const Text("Answer", style: TextStyle(fontSize: 30, color: Colors.blue), textAlign: TextAlign.center)
-      ),
-
-      SizedBox(width: MediaQuery.of(context).size.width, height: MediaQuery.of(context).size.height * 0.1, child:
-        Text(m_memory.m_answer, style: const TextStyle(fontSize: 30), textAlign: TextAlign.center)
-      ),
-
-      const Spacer(),
-
-      SizedBox(width: MediaQuery.of(context).size.width, height: MediaQuery.of(context).size.height * 0.2, child:
-        TextButton(onPressed: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context) => const PageHome()));
-        }, child: const Text("Home", style: TextStyle(fontSize: 30, color: Colors.blue))),
-      )
-    ])
+        SizedBox(width: MediaQuery.of(context).size.width, child:
+          TextButton(onPressed: () => onHomePressed(context), child:
+            const Text("Home", style: TextStyle(fontSize: 30, color: Colors.blue))
+          )
+        )
+      ])
     ));
+  }
+
+  void onHomePressed(BuildContext context)
+  {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => const PageHome()));
   }
 }
