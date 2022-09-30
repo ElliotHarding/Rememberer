c678621cdeef1d552f7968fe41f8781cf128f4c8
Elliot <elliot.one.harding@gmail.com>
2022-09-30 22:05:35 +0100

    wip -- progress chart

--- epilepsy_prevention/lib/page_memoryReminders.dart
+++ epilepsy_prevention/lib/page_memoryReminders.dart
@@ -90,7 +90,11 @@ class PageMemoryRemindersState extends State<PageMemoryReminders>
                       show: false,
                     ),
                     titlesData: FlTitlesData(
-                      show: false,
+                      show: true,
+                      bottomTitles: AxisTitles(sideTitles: SideTitles(showTitles: true)),
+                      leftTitles: AxisTitles(sideTitles: SideTitles(showTitles: true, reservedSize: 50)),
+                      rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                      topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false))
                     ),
                     scatterTouchData: ScatterTouchData(
                       enabled: true,
@@ -114,6 +118,45 @@ class PageMemoryRemindersState extends State<PageMemoryReminders>
         SizedBox(height: MediaQuery.of(context).size.height * 0.01),
        ])
     ));
+  }
+
+  Widget getTitles(double value, TitleMeta meta) {
+    const style = TextStyle(
+      color: Colors.blue,
+      fontSize: 15
+    );
+    Widget text;
+    switch (value.toInt()) {
+      case 0:
+        text = const Text('M', style: style);
+        break;
+      case 1:
+        text = const Text('T', style: style);
+        break;
+      case 2:
+        text = const Text('W', style: style);
+        break;
+      case 3:
+        text = const Text('T', style: style);
+        break;
+      case 4:
+        text = const Text('F', style: style);
+        break;
+      case 5:
+        text = const Text('S', style: style);
+        break;
+      case 6:
+        text = const Text('S', style: style);
+        break;
+      default:
+        text = const Text('', style: style);
+        break;
+    }
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      space: 16,
+      child: text,
+    );
   }
 
   void onCurrentIterationSliderChanged(double newValue)
