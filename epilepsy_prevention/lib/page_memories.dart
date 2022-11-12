ad4cb12c74d1a273bc48a0d2d0ac82debfe85e5c
elliotharding <elliot.one.harding@gmail.com>
2022-11-12 13:35:06 +0000

    wip - ui improvements

--- epilepsy_prevention/lib/page_memories.dart
+++ epilepsy_prevention/lib/page_memories.dart
@@ -29,20 +29,20 @@ class PageMemoriesState extends State<PageMemories>
     return WillPopScope(onWillPop: () async { Navigator.push(context, MaterialPageRoute(builder: (context) => const PageHome())); return true;}, child: Scaffold(body:
       ListView(shrinkWrap: true, children: <Widget>[
 
-        SizedBox(width: MediaQuery.of(context).size.width, child:
-          Row(children: [
-            Padding(padding: EdgeInsets.all(titlePadding), child:
-              SizedBox(width: MediaQuery.of(context).size.width * 0.6, child:
-                Align(alignment: Alignment.centerLeft, child: Text("Memories", style: Display.titleTextStyle, textAlign: TextAlign.left))
-            )),
+        Row(children: [
+          Padding(padding: EdgeInsets.all(titlePadding), child:
+            SizedBox(width: MediaQuery.of(context).size.width * 0.6, child:
+              Align(alignment: Alignment.centerLeft, child: Text("Memories", style: Display.titleTextStyle, textAlign: TextAlign.left))
+          )),
 
-            Padding(padding: EdgeInsets.fromLTRB(0, titlePadding, titlePadding, titlePadding), child: SizedBox(width: MediaQuery.of(context).size.width * 0.1, child:
-              TextButton(onPressed: () => gotoAddNewMemory(), child: Align(alignment: Alignment.centerRight, child: SizedBox(width: MediaQuery.of(context).size.width * 0.1, child:
-                Text("+", style: Display.titleTextStyle, textAlign: TextAlign.right)
-              ))),
-            ))
-          ])
-        ),
+          Padding(padding: EdgeInsets.fromLTRB(0, titlePadding, titlePadding, titlePadding), child: SizedBox(width: MediaQuery.of(context).size.width * 0.1, child:
+            TextButton(onPressed: () => gotoAddNewMemory(), child: Align(alignment: Alignment.centerRight, child: SizedBox(width: MediaQuery.of(context).size.width * 0.1, child:
+              Text("+", style: Display.titleTextStyle, textAlign: TextAlign.right)
+            ))),
+          ))
+        ]),
+
+        Visibility(visible: m_memoryWidgets.isEmpty, child: Text("No Memories Yet!", style: Display.largeTextStyle, textAlign: TextAlign.center,)),
 
         ListView(shrinkWrap: true, physics: const NeverScrollableScrollPhysics(), scrollDirection: Axis.vertical, children: m_memoryWidgets),
       ])
